6c9b62aaf5f9021c74e8d0ab168cfa4c560c6f09
xiaocong <1032852151@qq.com>
2022-04-24 18:24:45 +0800

    update Constants

--- src/test/java/com/kingsoft/data/Constants.java
+++ src/test/java/com/kingsoft/data/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 
     public static final String excelPath = "src/test/resources/api_testcases_futureloan_v4.xls";
     //项目的配置信息
-    public static final String BASE_URL = "http://39.99.240:224:8081/futureloan";
+    public static final String BASE_URL = "http://39.99.240.224:8081/futureloan";
     //数据库配置信息
     public static final String DB_BASE_URL = "39.99.240.224:3307";
     public static final String DB_NAME = "futureloan";
